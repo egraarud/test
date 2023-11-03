7dd2164772c7774fa49d646dbe92e7377c3b3b6f
egraarud <141819281+egraarud@users.noreply.github.com>
2023-11-03 11:19:58 +0100

    Add files via upload

---
+++ setup-vm.ps1
@@ -0,0 +1,10 @@
+#Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+#Install Software
+choco install sql-server-management-studio -y
+choco install visualstudiocode -y
+choco install git -y
+choco install powerbi -y
+choco install daxstudio -y
+choco install visualstudio2017professional --params "--all --includeRecommended --includeOptional --passive"
